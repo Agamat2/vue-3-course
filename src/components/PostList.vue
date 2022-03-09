29f75ec0c9e576c19e9db44149c66f42bd12c33c
Agamat <example@gmail.com>
2022-03-09 16:40:05 +0500

    mycommit

---
+++ src/components/PostList.vue
@@ -0,0 +1,31 @@
+<template>
+        <div class="post" v-for="post in posts" :key="post.id">
+            <div><strong>Ady:</strong>{{post.title}}</div>
+            <div><strong>Opisaniye:</strong>{{post.body}}</div>
+        </div>
+</template>
+
+<script>
+export default {
+    props:{
+        posts:{
+            type:Array,
+            required:true
+        }
+    },
+        data(){
+            return{
+                
+            }
+        }
+    
+}
+</script>
+
+<style scoped>
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+</style>
